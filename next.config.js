77fcdc3e1780d29cd8026b0ddbe86425515380f4
andrewgrablewski <agrablewski@gmail.com>
2021-12-01 01:12:20 -0500

    ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,40 @@
-module.exports = {
-  // Prefer loding of ES Modules over Common JS
-  experimental: { esmExternals: true },
-  build: {
-    extend(config, {}) {
-      config.node = {
-        fs: 'empty'
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase) => {
+
+  if(phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      experimental: { esmExternals: true },
+      build: {
+        extend(config, {}) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+      },
+      env: {
+        mongodb_username: 'andrew123',
+        mongodb_password: 'andrew123',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'nextjs-blog-dev'
       }
     }
   }
 
+  return {
+    experimental: { esmExternals: true },
+    build: {
+      extend(config, {}) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
+    },
+    env: {
+      mongodb_username: 'andrew123',
+      mongodb_password: 'andrew123',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'nextjs-blog'
+    }
+  }
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -25,8 +25,10 @@ async function handler(req, res){
 
         let client
 
+        const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.lzocf.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`
+
         try {
-            client = await MongoClient.connect('mongodb+srv://andrew123:andrew123@cluster0.lzocf.mongodb.net/nextjs-blog?retryWrites=true&w=majority')
+            client = await MongoClient.connect(connectionString)
         } catch (error) {
             res.status(500).json({message: 'Could not connect to database'})
             return 
